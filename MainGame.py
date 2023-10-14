49476b66a778a1690344f240708916ec3c3db0d7
Adeja Mann <mannadeja@gmail.com>
2023-10-13 21:46:18 -0400

    test

---
+++ MainGame.py
@@ -0,0 +1,7 @@
+import pygame
+from pygame.locals import *
+import pygame.examples.aliens as aliens
+aliens.main()
+# print("Hello world")
+# print("Best game!")
+
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DarkMode-HackGT
 Ai based game
+
